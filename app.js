a16226f70c40406fffc607ea52fbdf9a6111418f
mike vallano <mikevallano@gmail.com>
2018-08-26 14:12:33 -0500

    Connect to github api.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const searchInput = document.getElementById('search-user')
+const github = new GitHub()
 
 searchInput.addEventListener('keyup', (e) => {
   let searchVal = e.target.value
   if (searchVal !== '') {
-    console.log({searchVal})
+    github.getUser(searchVal)
+      .then(data => {
+        if (data.profileData.message == 'Not Found') {
+          console.log('not found: ', data)
+          // show user not found
+        } else {
+          // show user data
+          console.log({data})
+        }
+      })
+  } else {
+    // clear data
   }
 })
--- github.js
+++ github.js
@@ -2,5 +2,15 @@ class GitHub {
   constructor() {
     this.client_id = config.client_id
     this.client_secret = config.client_secret
+    this.base_url = 'https://api.github.com/users'
+  }
+
+  async getUser(userName) {
+    const profileRepsonse = await fetch(`${this.base_url}/${userName}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
+    const profileData = await profileRepsonse.json()
+
+    return {
+      profileData // same as profileData: profileData
+    }
   }
 }
