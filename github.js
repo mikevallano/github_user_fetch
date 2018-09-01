709d470b8ef2d14d5de159b447c939dffc914c27
mike vallano <mikevallano@gmail.com>
2018-09-01 09:06:19 -0500

    Refactor repo url

--- github.js
+++ github.js
@@ -5,14 +5,14 @@ class GitHub {
     this.base_url = 'https://api.github.com/users'
     this.repo_count = 5
     this.sort_order = 'created: asc'
+    this.credentials = `client_id=${this.client_id}&client_secret=${this.client_secret}`
   }
 
   async getUser(userName) {
-    const profileRepsonse = await fetch(`${this.base_url}/${userName}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
-    const repoResponse = await fetch(`${this.base_url}/${userName}/repos?per_page=${this.repo_count}&sort=${this.sort_order}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+    const profileRepsonse = await fetch(`${this.base_url}/${userName}?${this.credentials}`)
+    const repoResponse = await fetch(`${this.base_url}/${userName}/repos?per_page=${this.repo_count}&sort=${this.sort_order}&${this.credentials}`)
     const profileData = await profileRepsonse.json()
     const repoData = await repoResponse.json()
-
 
     return {
       profileData, // same as profileData: profileData
